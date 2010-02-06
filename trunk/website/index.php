80fb81d4a1a74ee7756275f7eb191fd999f5fd5f
weaverst <weaverst@699f617c-11bd-11df-a1e4-8def113ae3e8>
2010-02-06 00:25:51 +0000

    initial code for website

---
+++ trunk/website/index.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+    <link rel="stylesheet" href="/assets/css/960.css" type="text/css" media="screen" charset="utf-8">
+    <link rel="stylesheet" href="/assets/css/main.css" type="text/css" media="screen" charset="utf-8">
+</head>
+<body>
+    <div id="site" class="container_12">
+        <div id="navbar" class="grid_12">
+            <div id="titles" class="grid_2 alpha">
+               <a href="/">The Jolly Robo</a> 
+            </div>
+            <div id="mainmenu" class="grid_6">
+                <ul>
+                    <li>
+                        <a href="about">About</a>
+                    </li>
+                    <li>
+                        <a href="members">Members</a>
+                    </li>
+                    <li>
+                        <a href="designs">Designs</a>
+                    </li>
+                    <li>
+                        <a href="media">Media</a>
+                    </li>
+                    <li>
+                        <a href="contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <?php $uri = $_SERVER['REQUEST_URI'] !=  '/'? $_SERVER['REQUEST_URI'] : "/index" ?>
+        <?php $page = './pages' . $uri . '.html';?>
+        <?php 
+            if(file_exists($page)) { 
+                include $page; 
+            }
+            else {
+                $page = './pages/error.html';                   
+                include $page; 
+            }
+        ?>
+    </div>
+</body>
+</html>
