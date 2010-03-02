bab15ab571895a6517afee4796cd3650eaebcb90
weaverst <weaverst@699f617c-11bd-11df-a1e4-8def113ae3e8>
2010-03-02 23:09:59 +0000

    some stuff for plotting a line as a function of time. don't know how useful but might be cool

--- code/beagleboard/graph_follow.py
+++ code/beagleboard/graph_follow.py
@@ -1,24 +1,50 @@
-#!python
-
-#Our points that we want to get to
-x = [283,428,428,246,204,179,167,62,69,75,109,68,100,76,59]
-y = [61,79,214,230,244,269,308,309,142,110,93,84,70,61,40]
+#!/usr/bin/env python
 
 #calculate current position on graph, move based on plot
 import pylab
 import numpy as np
+import matplotlib
+import time 
 
-pylab.plot(x, y)
+matplotlib.use('TkAgg') # do this before importing pylab
 
-#Screw interpolation, we will do simple algebra instead
+#pylab.plot(x, y)
+fig = pylab.figure()
+ax = fig.add_subplot(111)
 
-#start between points 1 and 2
-for i in range(len(x) - 1):
-    #create linear function
-    for j in range(abs(x[i+1]-x[i])):
-        xc = x[i] + j
-        yc = y[i] + (xc-x[i])*(y[i+1] - y[i])/(x[i+1] - x[i]) 
-        pylab.plot(xc,yc,'bs')
+def animate():
+    x = [283,428,428,246,204,179,167,62,69,75,109,68,100,76,59]
+    y = [61,79,214,230,244,269,308,309,142,110,93,84,70,61,40]
 
+    tstart = time.time()                   # for profiling
+    line, = ax.plot(x, y)
+
+    for i in range(len(x) - 1):
+        for j in range(abs(x[i+1]-x[i])):
+            if x[i+1] < x[i]:
+                xc = x[i] - j
+            else:
+                xc = x[i] + j
+            yc = y[i] + (xc-x[i])*(y[i+1] - y[i])/(x[i+1] - x[i]) 
+            line.set_xdata(xc)
+            line.set_ydata(yc)                        # update the data
+            fig.canvas.draw()                         # redraw the canvas
+        print 'FPS:' , 200/(time.time()-tstart)
+
+
+win = fig.canvas.manager.window
+fig.canvas.manager.window.after(100, animate)
 pylab.show()
 
+
+##start between points 1 and 2
+#for i in range(len(x) - 1):
+#    #create linear function
+#    for j in range(abs(x[i+1]-x[i])):
+#        if x[i+1] < x[i]:
+#            xc = x[i] - j
+#        else:
+#            xc = x[i] + j
+#        yc = y[i] + (xc-x[i])*(y[i+1] - y[i])/(x[i+1] - x[i]) 
+#        #print xc,yc
+#        #pylab.plot(xc,yc)
