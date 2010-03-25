60eb3b08c9d4b2d43eee70fe95ef7c1f02c61696
weaverst <weaverst@699f617c-11bd-11df-a1e4-8def113ae3e8>
2010-03-25 19:04:19 +0000

    update to serial information

--- code/beagleboard/libs/sensorData.py
+++ code/beagleboard/libs/sensorData.py
@@ -1,23 +1,13 @@
 from xml.dom.minidom import parse, parseString
 
-#EXAMPLE DATA
-#<sensor>
-#  <gps>rawdata</gps>
-#  <compass>degree</compass>
-#  <flex>number</flex>
-#  <ultrasonic>inches</ultrasonic>
-#  <beacon>???</beacon>
-#  <wheelencoder>???</wheelencoder>
-#</sensor>
-
 class sensorData:
     def __init__(self,xml):
         self.xml = xml
         self.dom = parseString(self.xml)
         self.gps = self.get_data('gps') 
         self.compass = self.get_data('compass')
-        self.flex = self.get_data('flex')
-        self.us = self.get_data('ultrasonic')
+        self.flex = self.get_flex() 
+        self.us = self.get_us() 
         self.beacon = self.get_data('beacon')
         self.wheelencoder = self.get_data('wheelencoder')
 
@@ -34,6 +24,17 @@ class sensorData:
     def get_data(self,text):
         return self.get_text(self.dom.getElementsByTagName(text)[0].childNodes)
 
+    def get_flex(self):
+        flex = self.dom.getElementsByTagName('flex')[0]
+        left = self.get_text(flex.getElementsByTagName('left')[0].childNodes)       
+        right= self.get_text(flex.getElementsByTagName('right')[0].childNodes)
+        return dict(left=left,right=right)
+ 
+    def get_us(self):
+        us = self.dom.getElementsByTagName('ultrasonic')[0]
+        left = self.get_text(us.getElementsByTagName('left')[0].childNodes)       
+        right= self.get_text(us.getElementsByTagName('right')[0].childNodes)
+        return dict(left=left,right=right)
 
     def get_text(self,nodelist):
         rc = ""
@@ -44,6 +45,6 @@ class sensorData:
 
 if __name__ == '__main__':
     # Self-testing code goes here.
-    sxml= "<sensor><gps>rawdata</gps><compass>degree</compass><flex>number</flex><ultrasonic>inches</ultrasonic><beacon>???</beacon><wheelencoder>???</wheelencoder></sensor>"
+    sxml= "<sensor><gps>rawdata</gps><compass>degree</compass><flex><left>number</left><right>number</right></flex><ultrasonic><left>number</left><right>number</right></ultrasonic><beacon>???</beacon><wheelencoder>???</wheelencoder></sensor>"
     sd = sensorData(sxml)
     print sd.flex
