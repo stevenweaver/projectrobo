d4d0cfd6a1e1eae9af09c05c62a817906b3d7652
weaverst <weaverst@699f617c-11bd-11df-a1e4-8def113ae3e8>
2010-04-03 03:06:25 +0000

    some tests to do when we have beagleboard up on the robot

--- trunk/code/beagleboard/sensor_tests/flex_test.py
+++ trunk/code/beagleboard/sensor_tests/flex_test.py
@@ -1,20 +1,55 @@
-#For on robot or from flex
-#import serial
+from sendSerial import *
+from sensorData import *
+import serial
+import time 
 
-#ser = serial.SerialPort('/dev/ttyUSB0', 9600)
+#Set up
+#ser = serial.Serial('/dev/arduino', 9600)
+#print ser.readline()
 
-#For dev
-ser = open('../libs/sample_serial', 'r+')
+sxml= "<sensor><compass>140</compass><flex><left>500</left><right>300</right></flex><ultrasonic><left>3</left><right>10</right></ultrasonic><beacon>???</beacon><wheelencoder>???</wheelencoder></sensor>"
 sd = sensorData(sxml)
-print flex_check
+send = sendData()
 
-def flex_check(x): 
+while 1:
+    #sd.update(ser.readline())
 
-    result = {
-    'RED ALERT': lambda x: x > 5,
-      'b': lambda x: x + 7,
-      'c': lambda x: x - 2
-     }[value](x)
+    print sd.flex['left']
 
-return result
+    #Check right side
+    if sd.flex['right'] < 450:
+        #Turn right 
+        print "we are running against a right wall"
+        ser.send(send.createXml("left"))
+
+    elif sd.flex['right'] < 500: 
+        #Turn right 
+        print "we are scrapping against a right wall"
+        ser.send(send.createXml("left"))
+
+    elif sd.flex['right'] < 653:
+        #Turn right 
+        print "we are hitting against a right wall"
+        ser.send(send.createXml("left"))
+
+    #Check left side
+    if sd.flex['left'] < 400:
+        #Turn left
+        print "we are running against a left wall"
+        #WE might need to stop and reverse
+        ser.send(send.createXml("right"))
+
+    elif sd.flex['left'] < 450: 
+        #Turn left 
+        print "we are scrapping against a left wall"
+        ser.send(send.createXml("right"))
+
+    elif sd.flex['left'] < 603:
+        #Turn left 
+        print "we are hitting against a left wall"
+        ser.send(send.createXml("right"))
+
+
+    #time.sleep(.001)
+    time.sleep(1)
 
