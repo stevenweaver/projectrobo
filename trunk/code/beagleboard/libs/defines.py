31a0bba799ca8ae45664566d00cceac5442472d8
weaverst <weaverst@699f617c-11bd-11df-a1e4-8def113ae3e8>
2010-04-07 20:18:25 +0000

    beacon update

--- trunk/code/beagleboard/libs/defines.py
+++ trunk/code/beagleboard/libs/defines.py
@@ -1,4 +1,12 @@
+#Motor Constants
 STOP = -1 
 FORWARD = 0
 LEFT = 1
 RIGHT = 2
+
+#Beacon Constants
+NA = -1
+STRAIGHT = 0
+LEFT = 1
+RIGHT = 2
+
---
+++ trunk/code/beagleboard/sensor_tests/beacon_test.py
@@ -0,0 +1,55 @@
+from sendSerial import *
+from sensorData import *
+from defines import *
+import serial
+import time 
+import numpy as np
+
+#we should be receiving the servo position, if it's around the same position for any length of time, we should no to move in that direction. 
+
+#Set up
+#ser = serial.Serial('/dev/arduino', 9600)
+#print ser.readline()
+
+sxml= "<sensor><compass>140</compass><flex><left>500</left><right>300</right></flex><ultrasonic><left>3</left><right>10</right></ultrasonic><beacon>2</beacon><wheelencoder>???</wheelencoder></sensor>"
+sd = sensorData(sxml)
+send = sendData()
+
+#list of past positions
+#pos = np.zeros(10)
+pos = [] 
+
+#f = open('./log/beacontest_' + str(time.time()) , 'w')
+
+while 1:
+    #sd.update(ser.readline())
+
+    #print sd.beacon
+    #f.write(str(sd.beacon) + '\n')
+
+    pos.append(sd.beacon)
+    arr = np.array(pos)
+    mean = arr.mean() 
+    std = arr.std()
+
+
+    if mean < 85 and std < 5:
+        print "beacon is on the right, turn right" 
+        #ser.send(send.sendStr(RIGHT))
+
+    elif mean > 95 and std < 5:
+        #Turn left
+        print "beacon is on the left, turn left"
+        #ser.send(send.sendStr(LEFT))
+
+    elif mean > 85 and mean < 95 and std < 5:
+        #Turn right 
+        print "beacon is on the forward, turn forward"
+        #ser.send(send.sendStr(FORWARD))
+
+    
+    if len(pos) > 10:
+        del pos[0]
+
+    #time.sleep(.001)
+    time.sleep(1)
