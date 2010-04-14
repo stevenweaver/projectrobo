ae0243a0c5405a45c887913e8659dde5e81fb980
weaverst <weaverst@699f617c-11bd-11df-a1e4-8def113ae3e8>
2010-04-14 18:26:41 +0000

    updating xml code to have less characters

--- code/beagleboard/libs/sensorData.py
+++ code/beagleboard/libs/sensorData.py
@@ -4,34 +4,34 @@ class sensorData:
     def __init__(self,xml):
         self.xml = xml
         self.dom = parseString(self.xml)
-        self.compass = int(self.get_data('compass'))
+        self.compass = float(self.get_data('c'))
         self.flex = self.get_flex()
         self.us = self.get_us()
-        self.beacon = int(self.get_data('beacon'))
-        self.wheelencoder = self.get_data('wheelencoder')
+        self.beacon = int(self.get_data('b'))
+        self.wheelencoder = self.get_data('we')
 
     def update(self,xml):
         self.xml = xml
         self.dom = parseString(self.xml)
-        self.compass = int(self.get_data('compass'))
+        self.compass = float(self.get_data('c'))
         self.flex = self.get_flex()
         self.us = self.get_us()
-        self.beacon = self.get_data('beacon')
-        self.wheelencoder = self.get_data('wheelencoder')
+        self.beacon = self.get_data('b')
+        self.wheelencoder = self.get_data('we')
 
     def get_data(self,text):
         return self.get_text(self.dom.getElementsByTagName(text)[0].childNodes)
 
     def get_flex(self):
-        flex = self.dom.getElementsByTagName('flex')[0]
-        left = int(self.get_text(flex.getElementsByTagName('left')[0].childNodes))       
-        right= int(self.get_text(flex.getElementsByTagName('right')[0].childNodes))
+        flex = self.dom.getElementsByTagName('f')[0]
+        left = int(self.get_text(flex.getElementsByTagName('l')[0].childNodes))       
+        right= int(self.get_text(flex.getElementsByTagName('r')[0].childNodes))
         return dict(left=left,right=right)
  
     def get_us(self):
-        us = self.dom.getElementsByTagName('ultrasonic')[0]
-        left = int(self.get_text(us.getElementsByTagName('left')[0].childNodes)) 
-        right= int(self.get_text(us.getElementsByTagName('right')[0].childNodes))
+        us = self.dom.getElementsByTagName('us')[0]
+        left = int(self.get_text(us.getElementsByTagName('l')[0].childNodes)) 
+        right= int(self.get_text(us.getElementsByTagName('r')[0].childNodes))
         return dict(left=left,right=right)
 
     def get_text(self,nodelist):
@@ -43,7 +43,7 @@ class sensorData:
 
 if __name__ == '__main__':
     # Self-testing code goes here.
-    sxml= "<sensor><compass>140</compass><flex><left>500</left><right>300</right></flex><ultrasonic><left>10</left><right>10</right></ultrasonic><beacon>0</beacon><wheelencoder>???</wheelencoder></sensor>"
+    sxml= "<?xml version=\"1.0\"?><sensor><c>256.39</c><f><l>497</l><r>400</r></f><us><l>80</l><r>79</r></us><b>87</b><we><a>2000</a><b>1998</b></we></sensor>"
     sd = sensorData(sxml)
     print sd.flex['left']
     print sd.flex['right']
