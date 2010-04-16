c97e308dea21724ce54cf8303f2ee8c3b6dcec20
weaverst <weaverst@699f617c-11bd-11df-a1e4-8def113ae3e8>
2010-04-16 21:26:50 +0000

    latest main_2

--- trunk/code/beagleboard/libs/sendSerial.py
+++ trunk/code/beagleboard/libs/sendSerial.py
@@ -1,5 +1,6 @@
 from xml.dom.minidom import Document
 from sensorData import *
+from defines import *
 import serial
 import time
 
@@ -8,22 +9,42 @@ import time
 #<direction>right,left,forward,reverse</direction>
 
 class sendData:
-    def sendStr(self, direction, ticks):
+    def sendStr(self, direction):
         data = str(direction)
-        data += ","
-        data += str(ticks) 
-        data += "\n"
-        data = data.zfill(9)
+        #data += "\n"
+        data = data.zfill(10)
         return data 
 
 if __name__ == '__main__':
-    while(1):
-        ser = serial.Serial('/dev/arduino', 9600)
-        # Self-testing code goes here.
-        sxml= "<?xml version=\"1.0\"?><sensor><c>256.39</c><f><l>497</l><r>400</r></f><us><l>80</l><r>79</r></us><b>87</b><we><a>2000</a><b>1998</b></we></sensor>"
-        sd = sensorData(sxml)
-        send = sendData()
-        pis = send.sendStr(3,2000) 
-        ser.write("@003,567!")
-        time.sleep(.200)
-        print ser.readline()
+    ser = serial.Serial('/dev/arduino', 9600)
+    # Self-testing code goes here.
+    sxml= "<?xml version=\"1.0\"?><sensor><c>256.39</c><f><l>497</l><r>400</r></f><us><l>80</l><r>79</r></us><b>87</b><we><a>2000</a><b>1998</b></we></sensor>"
+    sd = sensorData(sxml)
+    send = sendData()
+
+    time.sleep(1)
+    ser.write(send.sendStr(STOP))
+
+    time.sleep(1)
+    ser.write(send.sendStr("1T"))
+    print ser.readline()
+    time.sleep(1)
+
+    ser.write(send.sendStr(LEFT))
+    print ser.readline()
+    time.sleep(10)
+
+    ser.write(send.sendStr(STOP))
+    print ser.readline()
+    time.sleep(1)
+
+    ser.write(send.sendStr("3T"))
+    print ser.readline()
+    time.sleep(1)
+
+    ser.write(send.sendStr(RIGHT))
+    print ser.readline()
+    time.sleep(10)
+
+    ser.write(send.sendStr(STOP))
+    print ser.readline()
