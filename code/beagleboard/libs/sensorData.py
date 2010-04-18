7655656ce5114abf076828195987282622a2a344
weaverst <weaverst@699f617c-11bd-11df-a1e4-8def113ae3e8>
2010-04-18 05:09:41 +0000

    main beagleboard code modifications

--- code/beagleboard/libs/sensorData.py
+++ code/beagleboard/libs/sensorData.py
@@ -4,20 +4,11 @@ class sensorData:
     def __init__(self,xml):
         self.xml = xml
         self.dom = parseString(self.xml)
-        self.compass = float(self.get_data('c')) / 10
-        self.flex = self.get_flex()
-        self.us = self.get_us()
-        self.beacon = int(self.get_data('b'))
-        self.wheelencoder = self.get_data('we')
-
-    def update(self,xml):
-        self.xml = xml
-        self.dom = parseString(self.xml)
         self.compass = float(self.get_data('c')) / 10 
         self.flex = self.get_flex()
         self.us = self.get_us()
         self.beacon = self.get_data('b')
-        self.wheelencoder = self.get_data('we')
+        self.dis_traveled = self.get_clicks()
 
     def get_data(self,text):
         return self.get_text(self.dom.getElementsByTagName(text)[0].childNodes)
@@ -34,6 +25,12 @@ class sensorData:
         right= int(self.get_text(us.getElementsByTagName('r')[0].childNodes))
         return dict(left=left,right=right)
 
+    def get_clicks(self):
+        we = self.dom.getElementsByTagName('we')[0]
+        a = int(self.get_text(us.getElementsByTagName('a')[0].childNodes)) 
+        b = int(self.get_text(us.getElementsByTagName('b')[0].childNodes))
+        return dict(left=left,right=right)
+
     def get_text(self,nodelist):
         rc = ""
         for node in nodelist:
@@ -44,6 +41,11 @@ class sensorData:
 if __name__ == '__main__':
     # Self-testing code goes here.
     sxml= "<?xml version=\"1.0\"?><sensor><c>256.39</c><f><l>497</l><r>400</r></f><us><l>80</l><r>79</r></us><b>87</b><we><a>2000</a><b>1998</b></we></sensor>"
-    sd = sensorData(sxml)
-    print sd.flex['left']
-    print sd.flex['right']
+    sd = []
+    sd.append(sensorData(sxml))
+    sd.append(sensorData(sxml))
+    print sd[0].flex['left']
+    print sd[0].flex['right']
+    print sd[1].flex['left']
+    print sd[1].flex['right']
+
