56fedb4b75010d16faea116b018256add56a76ff
weaverst <weaverst@699f617c-11bd-11df-a1e4-8def113ae3e8>
2010-04-21 02:17:58 +0000

    adding more beagleboard changes

--- trunk/code/beagleboard/setup.py
+++ trunk/code/beagleboard/setup.py
@@ -1,7 +1,11 @@
 #First we need to include the new python path
 import sys
 #QA
-HOME = '/home/steven/projectrobo/code/beagleboard' 
+#HOME = '/home/steven/projectrobo/code/beagleboard' 
+
+#HAPPYCAT
+HOME = '/home/steven/share/school/senior_design/projectrobo/code/beagleboard' 
+
 #BEAGLE
 #HOME = '/home/ubuntu/beagle' 
 sys.path.append(HOME + '/libs/') 
