c129d0ca2ae90fff382fb49b7376ed04fbc99eb6
weaverst <weaverst@699f617c-11bd-11df-a1e4-8def113ae3e8>
2010-04-25 18:23:43 +0000

    atwaypoint changes

--- trunk/code/beagleboard/objects/path_find.py
+++ trunk/code/beagleboard/objects/path_find.py
@@ -7,14 +7,16 @@ class pathFind:
         self.y = [170, 170, 66, 66, 55, 50, 50, 42, 42, 10, 10, 87, 87, 120, 127, 135, 135, 140, 150, 152, 167]
         self.waypoints = zip(x,y)
 
+        self.current_point = ()
+
         self.waypoint_count = 0
         self.beacon_count = 1
 
-    def goTowardsNewDestination():
+    def goTowardsNewDestination(sd):
         #this gets called when we need to calculate the next stop we should go to
         #Decide how to get there
         motor.goDir(STOP)
-        dr_current_point = dead_reckon.calcPosition()
+        dr_current_point = dead_reckon.calcPosition(sd)
         
         #Find distance and bearing to next position
         distance = (current_point, next_waypoint)
@@ -28,8 +30,16 @@ class pathFind:
 
     #waypoint matching using dead-reckoning
     #return: 0 if not, 1 if at waypoint
-    def atWaypoint(wp):
-        current_point = calcPosition()
-        if (math.hypot(current_point[0] + current_point[1]) == math.hypot(wp[0] + wp[1])):
-            return 1
+    def atWaypoint(self,sd):
+        last_waypoint = self.waypoints[self.waypoint_count]
+        next_waypoint = self.waypoints[self.waypoint_count + 1]
+        #This is NOT the end all, be all to waypoint detection, this is the dead reckoning and compass portion. 
+        current_point = dead_reckon.calcPosition(sd)
+        #we have to use dead reckoning and compass information in order to get a good idea of where we have gone
+        if abs(next_waypoint[0] - last_waypoint[0]) > abs(next_waypoint[1] - last_waypoint[1]):
+            if current_point[0] > next_waypoint[0]:
+                return 1 
+        else:
+            if current_point[1] > next_waypoint[1]:
+                return 1
         return 0
