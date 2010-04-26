adcdedd3c5fa9ccee5ec70e2009bb3ef80b77017
weaverst <weaverst@699f617c-11bd-11df-a1e4-8def113ae3e8>
2010-04-26 05:36:10 +0000

    qa main

---
+++ code/beagleboard/qa_main.py
@@ -0,0 +1,50 @@
+#Import the modules we need
+import setup
+import motor
+import avoidance
+import beacon
+import path_find
+import comm
+from defines import * 
+import numpy as np
+import time
+
+#main loop 
+def main():
+    #initialization, our huge lists of information
+    ser = comm.comm()
+    sensor_data = []
+    gps_list = [] 
+    rssi_list = [] 
+    current_position = [] 
+    position_list = []
+    pf = path_find.pathFind()
+
+    #keep the time since we've started, could be useful to use along with wheel encoder information if we know how fast yertle goes ;)
+    time_started = time.time()
+
+    while(1):
+        sensor_data.insert(0,ser.updateSensors())
+        #gps_list.insert(0,ser.updateGps())
+        #rssi_list.insert(0,ser.updateRssi())
+
+        #Check if there are obstacles
+        #If there are obstacles, move out of the way. Once we feel safe, update new point on map and recreate interpolation to include new point where we sit.
+        #If obstacle, stop. Figure out which way to go, then try to go in the right direction again. 
+        if avoidance.obstacle(sensor_data[0]):
+            avoidance.moveOutTheWay(sensor_data[0])
+
+        #Check if there are Beacons
+        #Change direction to accommodate going through the beacon.
+        #Use rssi for proximity and override main path, this needs to send the whole list instead of just one 
+        elif beacon.beaconDetect(rssi_list[0]):
+           beacon.goTowardsBeacon(sensor_data[0]) 
+
+        #Otherwise continue going about our way
+        #wayPoint is the new calcPosition()
+        elif pf.atWaypoint(sensor_data):
+            pf.waypoint_count+=1
+            pf.goTowardsNewDestination()
+    return
+
+main()
