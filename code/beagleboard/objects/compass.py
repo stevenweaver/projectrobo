92faf39b3244b8338b564b68d150ec3c49d45c36
weaverst <weaverst@699f617c-11bd-11df-a1e4-8def113ae3e8>
2010-04-27 01:09:05 +0000

    compass update

--- code/beagleboard/objects/compass.py
+++ code/beagleboard/objects/compass.py
@@ -1,23 +1,25 @@
-#Compaass
-def checkCompass(desired_degree):
+#Compass
+def checkCompass(desired_degree, sd):
     #Make sure we have a decent sample rate
     #Reply back with veering right, veering left, going straight
     sum = 0
-    if abs(sd.compass[0] - desired_degree) > 10:
+    if abs(sd[9].compass - desired_degree) > 10:
         #TODO:Make sure it's not a blip
         if len(sd) > 10: 
             for i in range(10):
                 #Sum up the differences
-                sum+= (sd.compass[0] - desired_degree + 360) % 360
+                sum+= (sd[i].compass - desired_degree + 360) % 360
 
             avg = sum/10
             if avg > 180:
                 #Veering off left 
-                return "left"
+                print "compass veering left"
+                return 1 
             elif avg > 3:
                 #Veering off right 
-                return "right"
+                print "compass veering right"
+                return 1 
             else:
                 #Going straight enough
-                return "good"
+                return 0 
 
