43cd473a7ccad807de403889db17598a05f0ef98
weaverst <weaverst@699f617c-11bd-11df-a1e4-8def113ae3e8>
2010-04-27 01:10:04 +0000

    compass update

--- trunk/code/beagleboard/generate_xml.py
+++ trunk/code/beagleboard/generate_xml.py
@@ -5,22 +5,22 @@ import random
 f = open('./test_xml' , 'w')
 com = open('./sensor_tests/log/course1', 'r')
 for i in range(850):
-    sxml= '<?xml version="1.0"?><sensor><c>' + com.readline().rstrip('\n') + '</c><f><l>700</l><r>700</r></f><us><l>80</l><r>80</r></us><b>' + str(((i + 5) % 180))  + '</b></sensor>\n'
+    sxml= '<?xml version="1.0"?><sensor><c>' + com.readline().rstrip('\n') + '</c><f><l>700</l><r>700</r></f><us><l>80</l><r>80</r></us><b>' + str(((i + 5) % 180))  + '</b></sensor>'
     if i > 500 and i < 550: 
         #Scrape against the left hand side
-        sxml= '<?xml version="1.0"?><sensor><c>' + com.readline().rstrip('\n') + '</c><f><l>' + str(random.randint(400, 500)) + '</l><r>700</r></f><us><l>80</l><r>80</r></us><b>' + str(((i + 5) % 180))  + '</b></sensor>\n'
+        sxml= '<?xml version="1.0"?><sensor><c>' + com.readline().rstrip('\n') + '</c><f><l>' + str(random.randint(400, 500)) + '</l><r>700</r></f><us><l>80</l><r>80</r></us><b>' + str(((i + 5) % 180))  + '</b></sensor>'
 
     if i > 300 and i < 350: 
         #Scrape against the right hand side
-        sxml= '<?xml version="1.0"?><sensor><c>' + com.readline().rstrip('\n') + '</c><f><l>700</l><r>' + str(random.randint(400, 500)) + '</r></f><us><l>80</l><r>80</r></us><b>' + str(((i + 5) % 180))  + '</b></sensor>\n'
+        sxml= '<?xml version="1.0"?><sensor><c>' + com.readline().rstrip('\n') + '</c><f><l>700</l><r>' + str(random.randint(400, 500)) + '</r></f><us><l>80</l><r>80</r></us><b>' + str(((i + 5) % 180))  + '</b></sensor>'
 
     if i > 400 and i < 450: 
         #Get too close to an object
-        sxml= '<?xml version="1.0"?><sensor><c>' + com.readline().rstrip('\n') + '</c><f><l>700</l><r>700</r></f><us><l>' + str(i*-1+454) + '</l><r>' + str(i*-1+454) + '</r></us><b>' + str(((i + 5) % 180))  + '</b></sensor>\n'
+        sxml= '<?xml version="1.0"?><sensor><c>' + com.readline().rstrip('\n') + '</c><f><l>700</l><r>700</r></f><us><l>' + str(i*-1+454) + '</l><r>' + str(i*-1+454) + '</r></us><b>' + str(((i + 5) % 180))  + '</b></sensor>'
 
     #Lock on to a beacon
     if i > 700 and i < 750: 
-        sxml= '<?xml version="1.0"?><sensor><c>' + com.readline().rstrip('\n') + '</c><f><l>700</l><r>700</r></f><us><l>80</l><r>80</r></us><b>' + str(random.randint(115,125))  + '</b></sensor>\n'
+        sxml= '<?xml version="1.0"?><sensor><c>' + com.readline().rstrip('\n') + '</c><f><l>700</l><r>700</r></f><us><l>80</l><r>80</r></us><b>' + str(random.randint(115,125))  + '</b></sensor>'
 
     print sxml
     #f.write(sxml)
--- trunk/code/beagleboard/qa_main.py
+++ trunk/code/beagleboard/qa_main.py
@@ -3,10 +3,11 @@ import setup
 import motor
 import avoidance
 import beacon
+import compass
 import path_find
 import comm
 from defines import * 
-import numpy as np
+#import numpy as np
 import time
 
 #main loop 
@@ -22,21 +23,18 @@ def main():
 
     #keep the time since we've started, could be useful to use along with wheel encoder information if we know how fast yertle goes ;)
     time_started = time.time()
-    while(1):
-        tmp_data = 0
-        while tmp_data == 0:
-            tmp_data = ser.updateSensors()
-
-        sensor_data.insert(0,tmp_data)
-
+    #while(1):
+    for i in range(1000):
+        sensor_data.insert(0,ser.updateSensors())
         #gps_list.insert(0,ser.updateGps())
         #rssi_list.insert(0,ser.updateRssi())
 
         #Check if there are obstacles
         #If there are obstacles, move out of the way. Once we feel safe, update new point on map and recreate interpolation to include new point where we sit.
         #If obstacle, stop. Figure out which way to go, then try to go in the right direction again. 
-        if avoidance.obstacle(sensor_data[0]):
-            avoidance.moveOutTheWay(sensor_data[0])
+        #if avoidance.obstacle(sensor_data[0]) == 1:
+        #    print i
+        #    avoidance.moveOutTheWay(sensor_data[0])
 
         #Check if there are Beacons
         #Change direction to accommodate going through the beacon.
@@ -44,13 +42,19 @@ def main():
         #if beacon.beaconDetect(rssi_list[0]):
         #   beacon.goTowardsBeacon(sensor_data[0]) 
 
+        #Check if we're veering left or right
+        if len(sensor_data) > 10:
+            print sensor_data[0].compass 
+            if compass.checkCompass(sensor_data[0].compass,sensor_data[0:10]):
+                print "compass_moving"
+
         #Otherwise continue going about our way
         #wayPoint is the new calcPosition()
         #if pf.atWaypoint(sensor_data):
             #pf.waypoint_count+=1
             #pf.goTowardsNewDestination()
 
-        time.sleep(.02)
+        #time.sleep(.02)
     return
 
 main()
