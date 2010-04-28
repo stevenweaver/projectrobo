fad770efa314d3a0162836648389cf18fa1c2f69
weaverst <weaverst@699f617c-11bd-11df-a1e4-8def113ae3e8>
2010-04-28 05:29:30 +0000

    compass stuff

--- trunk/code/beagleboard/objects/compass.py
+++ trunk/code/beagleboard/objects/compass.py
@@ -3,23 +3,28 @@ def checkCompass(desired_degree, sd):
     #Make sure we have a decent sample rate
     #Reply back with veering right, veering left, going straight
     sum = 0
-    if abs(sd[9].compass - desired_degree) > 10:
+    if abs(sd[8].compass - desired_degree) > 10:
         #TODO:Make sure it's not a blip
-        if len(sd) > 10: 
+        if len(sd) >= 10: 
             for i in range(10):
                 #Sum up the differences
-                sum+= (sd[i].compass - desired_degree + 360) % 360
+                if i > 0:
+                    sum+= abs(sd[i].compass - desired_degree + 360) % 360
 
             avg = sum/10
+            print "avg: " + str(avg)
             if avg > 180:
                 #Veering off left 
                 print "compass veering left"
                 return 1 
-            elif avg > 3:
+            elif avg > 20:
                 #Veering off right 
                 print "compass veering right"
                 return 1 
             else:
                 #Going straight enough
+                print "compass going straight"
                 return 0 
+    else:
+        print "compass going straight"
 
--- trunk/code/beagleboard/plot.py
+++ trunk/code/beagleboard/plot.py
@@ -10,44 +10,51 @@ from defines import *
 #import numpy as np
 import time
 
+import pylab
+import numpy as np
+import matplotlib
+import time 
+from pylab import imread, imshow, gray, mean
+import math
+
+fig = pylab.figure()
+ax = fig.add_subplot(111)
+compass = []
+
 #main loop 
 def main():
     #initialization, our huge lists of information
     ser = comm.comm()
-    sensor_data = []
     gps_list = [] 
     rssi_list = [] 
     current_position = [] 
     position_list = []
     pf = path_find.pathFind()
+    sensor_data = []
 
     #keep the time since we've started, could be useful to use along with wheel encoder information if we know how fast yertle goes ;)
     time_started = time.time()
     #while(1):
-    for i in range(600):
+    for i in range(3000):
         sd = ser.updateSensors()
         if sd:
             sensor_data.insert(0,sd)
+            compass.append(sd.compass)
         #gps_list.insert(0,ser.updateGps())
         #rssi_list.insert(0,ser.updateRssi())
 
         #Check if there are obstacles
         #If there are obstacles, move out of the way. Once we feel safe, update new point on map and recreate interpolation to include new point where we sit.
         #If obstacle, stop. Figure out which way to go, then try to go in the right direction again. 
-        if avoidance.obstacle(sensor_data[0]) == 1:
-            avoidance.moveOutTheWay(sensor_data[0])
+        #if avoidance.obstacle(sensor_data[0]) == 1:
+        #    print i
+        #    avoidance.moveOutTheWay(sensor_data[0])
 
         #Check if there are Beacons
         #Change direction to accommodate going through the beacon.
         #Use rssi for proximity and override main path, this needs to send the whole list instead of just one 
         #if beacon.beaconDetect(rssi_list[0]):
         #   beacon.goTowardsBeacon(sensor_data[0]) 
-
-        #Check if we're veering left or right
-        if len(sensor_data) >= 10:
-            if compass.checkCompass(sensor_data[0].compass,sensor_data[0:10]):
-                #Correct ourselves
-                print "compass_moving"
 
         #Otherwise continue going about our way
         #wayPoint is the new calcPosition()
@@ -59,3 +66,10 @@ def main():
     return
 
 main()
+
+i = len(compass) - 1
+
+ax.plot(compass,'bo')                        # update the data
+fig.canvas.draw()                         # redraw the canvas
+win = fig.canvas.manager.window
+pylab.show()
