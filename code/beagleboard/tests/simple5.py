fa3c4404afa060ba6f798d062ad846edc3966d66
weaverst <weaverst@699f617c-11bd-11df-a1e4-8def113ae3e8>
2010-05-06 12:19:56 +0000

    changes

--- code/beagleboard/tests/simple5.py
+++ code/beagleboard/tests/simple5.py
@@ -36,26 +36,7 @@ def main():
     wheels = []
 
     starting_degree = -1 
-    command_count = 0
     bearing = 0 
-
-    command_queue = [] 
-    compass_commands = []
-    command_queue.append(('go',0,STOP))
-    command_queue.append(('go',10,FORWARD))
-    command_queue.append(('go',10,FORWARD))
-    command_queue.append(('go',10,FORWARD))
-    command_queue.append(('go',10,FORWARD))
-    command_queue.append(('go',10,FORWARD))
-    command_queue.append(('go',10,FORWARD))
-    command_queue.append(('go',10,FORWARD))
-    command_queue.append(('go',10,FORWARD))
-    command_queue.append(('go',10,FORWARD))
-    command_queue.append(('go',10,FORWARD))
-    command_queue.append(('go',10,FORWARD))
-
-    command = command_queue.pop(0)
-    motor.execute(command)
 
     sd = ser.updateSensors()
     while sd == 0:
@@ -71,80 +52,48 @@ def main():
 
         ser.ard_ser.flushInput()
         sd = ser.updateSensors()
-
-        #ser.rssi_ser.flushInput()
-        rssi_d = ser.updateRssi()
-
-        if rssi_d:
-            if rssi_d.RxNumber == beacon_number:
-                rssi_distance_list.insert(0,rssi_d.distance)
-
-                if len(rssi_distance_list) > 10:
-                    print rssi_distance_list
-                    dis_avg = mean(rssi_distance_list)
-                    rssi_distance_list.pop()
-
         if sd:
             sensor_data.insert(0,sd)
         if wheel_info:
             wheels.insert(0,wheel_info)
-            
-        #wall walking
-        while (sensor_data[0].us['r'] < 30)&(wheels[0].done_flags['right'] != 1 and wheels[0].done_flags['left'] != 1):
-            if (sensor_data[0].us['l'] < 25):
-                response = motor.execute(('turn', LEFT, 90))
-            elif (sensor_data[0].us['r'] < 10):    
-                response = motor.execute(('turn', LEFT, 20))
-            elif (sensor_data[0].us['r'] > 20):
-                response = motor.execute(('turn', RIGHT, 15))
-            time.sleep(1)
-            while wheels[0].done_flags['right'] != 1 and wheels[0].done_flags['left'] != 1: 
-            response = motor.execute(('go',3, FORWARD))
-                                    
 
-        elif going_towards_beacon:
+        print 'left', sensor_data[0].us['left']
+
+        if sensor_data[0].us['left'] < 10: 
             response = motor.execute(('go',0,STOP))
-            response = motor.execute(('go',1,FORWARD))
             time.sleep(1)
-            while wheels[0].done_flags['right'] != 1 and wheels[0].done_flags['left'] != 1: 
-                wheel_info = ser.updateWheel()
-                if wheel_info:
-                    wheels.insert(0,wheel_info)
-
-            ser.ard_ser.flushInput()
-            if beacon.underBeacon(dis_avg):
-                going_towards_beacon = 0
-                print "going towards beacon", going_towards_beacon
-
-            if beacon.stillLocked(sensor_data) != 1: 
-                going_towards_beacon = 0
-                print "going towards beacon", going_towards_beacon
-
+            response = motor.execute(('turn',LEFT,90))
+            time.sleep(1)
+             
         if wheels[0].done_flags['right'] == 1 and wheels[0].done_flags['left'] == 1: 
-            #execute the last command
-            bearing = distance.calcRelBearing(starting_degree, sensor_data[0].compass)
+            #wall walking
+            if (sensor_data[0].us['left'] < 25):
+                response = motor.execute(('turn', LEFT, 90))
+                while wheels[0].done_flags['right'] != 1 and wheels[0].done_flags['left'] != 1: 
+                    wheel_info = ser.updateWheel()
+                    if wheel_info:
+                        wheels.insert(0,wheel_info)
+            elif (sensor_data[0].us['right'] < 10):    
+                response = motor.execute(('turn', LEFT, 20))
+                while wheels[0].done_flags['right'] != 1 and wheels[0].done_flags['left'] != 1: 
+                    wheel_info = ser.updateWheel()
+                    if wheel_info:
+                        wheels.insert(0,wheel_info)
+            elif (sensor_data[0].us['right'] > 20):
+                response = motor.execute(('turn', RIGHT, 15))
+                while wheels[0].done_flags['right'] != 1 and wheels[0].done_flags['left'] != 1: 
+                    wheel_info = ser.updateWheel()
+                    if wheel_info:
+                        wheels.insert(0,wheel_info)
 
-            if abs(bearing) > 10:
-                if bearing > 10:
-                    #compass_commands.append(('go',0,STOP))
-                    compass_commands = ('turn', RIGHT, bearing)
-                    response = motor.execute(compass_commands)
+            time.sleep(1)
+            ser.babyard.flushInput()
 
-                if bearing < -10:
-                    #compass_commands.append(('go',0,STOP))
-                    compass_commands = ('turn', LEFT, -1*bearing)
-                    response = motor.execute(compass_commands)
+            wheel_info = ser.updateWheel()
+            if wheel_info:
+                wheels.insert(0,wheel_info)
 
-                compass_commands = () 
-                bearing = 0
 
-            elif command_queue:
-                command = command_queue.pop(0)
-                response = motor.execute(command)
-
-            else:
-                print 'finished'
-                f.write('finished')
-                quit()
+            response = motor.execute(('go',3, FORWARD))
     return
 main()
